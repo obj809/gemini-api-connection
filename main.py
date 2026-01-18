a4b129c19f6199fcc800da56f25bd8524c2770b9
Your Name <cyberforge1@gmail.com>
2026-01-18 12:20:23 +1100

    addition of project files

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print('hello world  ')
+# main.py
 
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
+MODEL = "gemini-2.5-flash"
+
+client = genai.Client()
+
+response = client.models.generate_content(
+    model=MODEL,
+    contents="Explain how AI works in a few words."
+)
+
+print(response.text)
